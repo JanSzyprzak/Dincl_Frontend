e9c398b0ab31f5e4b49509e4b4e4558c48453fe3
Jan Szyprzak <jan.szyprzak@gmail.com>
2023-09-05 19:18:21 +0200

    Add form validation

--- dincl_frontend/src/components/FillSurvey.jsx
+++ dincl_frontend/src/components/FillSurvey.jsx
@@ -1,90 +1,191 @@
-import React, { useState } from 'react';
-import { AGE_CHOICES, GENDER_CHOICES, VOIVODSHIP_CHOICES, CITY_SIZE_CHOICES, GROUP_CHOICES, RATING_CHOICES } from '../constants/Choices.jsx';
-import '../index.css';
+import React, { useState } from "react";
+import { Form, Button } from "react-bootstrap";
+
+import { FORM_FIELDS } from "../constants/Choices.jsx";
+
+import "../index.css";
 
 const FillSurveyComponent = () => {
-
-  const [formData, setFormData] = useState({
-    age: '',
-    gender: '',
-    voivodship: '',
-    city_size: '',
-    group: '',
-    identification_with_group: '',
-    identification_with_minority: '',
-    group_diversity: '',
-    ease_of_joining: '',
-    rule_fairness: '',
-    minority_participation_in_life: '',
-    minority_participation_in_decisions: '',
-    minority_potential_utilization: '',
-    personal_security_feeling: '',
-    minority_security_feeling: '',
-  });
-
-  const handleFormSubmit = (event) => {
-    event.preventDefault();
-    // Here, you can submit the form data to the server or handle it as needed
-    console.log(formData);
-  };
-
-  const handleChange = (event) => {
-    const { name, value } = event.target;
-    setFormData({
-      ...formData,
-      [name]: value,
+    const [formData, setFormData] = useState({
+        age: "",
+        gender: "",
+        voivodship: "",
+        city_size: "",
+        group: "",
+        identification_with_group: "",
+        identification_with_minority: "",
+        group_diversity: "",
+        ease_of_joining: "",
+        rule_fairness: "",
+        minority_participation_in_life: "",
+        minority_participation_in_decisions: "",
+        minority_potential_utilization: "",
+        personal_security_feeling: "",
+        minority_security_feeling: "",
     });
-  };
 
-  return (
-    <div>
-      <form onSubmit={handleFormSubmit}>
+    const [currentStep, setCurrentStep] = useState(1);
+    const [invalidFields, setInvalidFields] = useState([]);
+
+    const handleFormSubmit = (event) => {
+        event.preventDefault();
+
+        // Define the URL where the backend is expecting the POST request.
+        // Update this to your backend's URL
+        const backendUrl = "http://localhost:8000/api/v1/create-survey/";
+        console.log(formData);
+        // Use fetch API to make the POST request
+        fetch(backendUrl, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(formData),
+        })
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error("Network response was not ok");
+                }
+                return response.json();
+            })
+            .then((data) => {
+                console.log("Success:", data);
+                // Handle the response data or update state as needed
+            })
+            .catch((error) => {
+                console.error("Error:", error);
+                // Handle any errors that occur during the fetch
+            });
+    };
+
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        setFormData({
+            ...formData,
+            [name]: value,
+        });
+    };
+
+    const getFieldsForStep = (step) => {
+        switch (step) {
+            case 1:
+                return STEP1_FIELDS;
+            case 2:
+                return STEP2_FIELDS;
+            default:
+                return [];
+        }
+    };
+
+    const getInvalidFields = (stepFields) => {
+        let invalidFields = [];
+        for (let field of stepFields) {
+            if (!formData[field.name]) {
+                invalidFields.push(field.name); // Push the field name if it's empty
+            }
+        }
+        return invalidFields;
+    };
+
+    const handleNext = () => {
+        const currentInvalidFields = getInvalidFields(STEP1_FIELDS);
+        setInvalidFields(currentInvalidFields);
+        if (currentInvalidFields.length === 0) {
+            setCurrentStep(2);
+        }
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const currentInvalidFields = getInvalidFields(STEP2_FIELDS);
+        setInvalidFields(currentInvalidFields);
+        if (currentInvalidFields.length === 0) {
+            // ... proceed with form submission logic
+        }
+    };
+
+    const STEP1_FIELDS = FORM_FIELDS.slice(0, 5);
+    const STEP2_FIELDS = FORM_FIELDS.slice(5);
+
+    return (
         <div>
-          <label>Wiek</label>
-          <select
-            name="age"
-            value={formData.age}
-            onChange={handleChange}
-          >
-            <option value="">Select an option</option>
-            {AGE_CHOICES.map(({ value, label }) => (
-              <option key={value} value={value}>{label}</option>
-            ))}
-          </select>
+            <Form onSubmit={handleFormSubmit}>
+                {getFieldsForStep(currentStep).map((field) => (
+                    <div key={field.name}>
+                        <Form.Group
+                            className="d-flex justify-content-between mb-3"
+                            key={field.name}
+                        >
+                            <Form.Label
+                                style={{ marginRight: "20px" }}
+                                className="flex-grow-1"
+                            >
+                                {field.label}
+                            </Form.Label>
+                            <div
+                                className="flex-grow-2"
+                                style={{ width: "200px" }}
+                            >
+                                <Form.Control
+                                    as="select"
+                                    name={field.name}
+                                    value={formData[field.name]}
+                                    onChange={handleChange}
+                                    className={
+                                        invalidFields.includes(field.name)
+                                            ? "is-invalid"
+                                            : ""
+                                    }
+                                >
+                                    <option value="">Wybierz z listy</option>
+                                    {field.choices.map(({ value, label }) => (
+                                        <option key={value} value={value}>
+                                            {label}
+                                        </option>
+                                    ))}
+                                </Form.Control>
+                                {invalidFields.includes(field.name) && (
+                                    <div className="invalid-feedback">
+                                        To pole jest wymagane.
+                                    </div>
+                                )}
+                            </div>
+                        </Form.Group>
+                    </div>
+                ))}
+                <div className="text-center mt-3">
+                    {currentStep === 1 && (
+                        <button
+                            type="button"
+                            className="btn btn-primary"
+                            onClick={handleNext}
+                        >
+                            Next
+                        </button>
+                    )}
+
+                    {currentStep === 2 && (
+                        <>
+                            <button
+                                type="button"
+                                className="btn btn-secondary mr-2"
+                                onClick={() => setCurrentStep(1)}
+                            >
+                                Previous
+                            </button>
+                            <button
+                                type="submit"
+                                className="btn btn-primary"
+                                onClick={handleSubmit}
+                            >
+                                Submit
+                            </button>
+                        </>
+                    )}
+                </div>
+            </Form>
         </div>
-        <div>
-          <label>Płeć</label>
-          <select
-            name="gender"
-            value={formData.gender}
-            onChange={handleChange}
-          >
-            <option value="">Select an option</option>
-            {GENDER_CHOICES.map(({ value, label }) => (
-              <option key={value} value={value}>{label}</option>
-            ))}
-          </select>
-        </div>
-        <div>
-          <label>Województwo</label>
-          <select
-            name="voivodship"
-            value={formData.voivodship}
-            onChange={handleChange}
-          >
-            <option value="">Select an option</option>
-            {VOIVODSHIP_CHOICES.map(({ value, label }) => (
-              <option key={value} value={value}>{label}</option>
-            ))}
-          </select>
-        </div>
-        {/* Add other form fields using the same pattern */}
-        <div>
-          <button type="submit">Submit</button>
-        </div>
-      </form>
-    </div>
-  );
+    );
 };
 
 export default FillSurveyComponent;
---
+++ dincl_frontend/src/constants/Form_Fields.jsx
@@ -0,0 +1,61 @@
+export const FORM_FIELDS = [
+    { name: "age", label: "Wiek", choices: AGE_CHOICES },
+    { name: "gender", label: "Płeć", choices: GENDER_CHOICES },
+    {
+        name: "voivodship",
+        label: "Województwo",
+        choices: VOIVODSHIP_CHOICES,
+    },
+    { name: "city_size", label: "Miasto", choices: CITY_SIZE_CHOICES },
+    { name: "group", label: "Grupa", choices: GROUP_CHOICES },
+    {
+        name: "identification_with_group",
+        label: "Identyfikacja z grupą",
+        choices: RATING_CHOICES,
+    },
+    {
+        name: "identification_with_minority",
+        label: "Identyfikacja z mniejszością",
+        choices: RATING_CHOICES,
+    },
+    {
+        name: "group_diversity",
+        label: "Zróżnicowanie grupy",
+        choices: RATING_CHOICES,
+    },
+    {
+        name: "ease_of_joining",
+        label: "Łatwość dołączania",
+        choices: RATING_CHOICES,
+    },
+    {
+        name: "rule_fairness",
+        label: "Sprawiedliwość zasad",
+        choices: RATING_CHOICES,
+    },
+    {
+        name: "minority_participation_in_life",
+        label: "Udział mniejszości w życiu",
+        choices: RATING_CHOICES,
+    },
+    {
+        name: "minority_participation_in_decisions",
+        label: "Udział mniejszości w decyzjach",
+        choices: RATING_CHOICES,
+    },
+    {
+        name: "minority_potential_utilization",
+        label: "Wykorzystanie potencjału mniejszości",
+        choices: RATING_CHOICES,
+    },
+    {
+        name: "personal_security_feeling",
+        label: "Poczucie bezpieczeństwa osobiście",
+        choices: RATING_CHOICES,
+    },
+    {
+        name: "minority_security_feeling",
+        label: "Poczucie bezpieczeństwa mniejszości",
+        choices: RATING_CHOICES,
+    },
+];
